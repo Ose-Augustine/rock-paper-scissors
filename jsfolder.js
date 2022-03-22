32137b50811ce5f390bac9b056801280ff8313e9
Ose-Augustine <osebeckley@gmail.com>
2022-03-22 15:07:11 +0100

    refactored the event listeners to fire in one for loop

--- jsfolder.js
+++ jsfolder.js
@@ -24,35 +24,23 @@ function playRound(playerSelection,computerSelection=computerPlay()){
     }
     
 }
-function automation(){
-    let list=['rock','paper','scissors'];
-    for(char of list){
-        for (let i=0;i < 5;i++){
-            playRound(char);
-        }
-    }
-}
+
 const div=document.querySelector("div");
 const rock=document.createElement("div");
 const paper=document.createElement("div");
 const scissors=document.createElement("div");
+const list=['rock','paper','scissors'];
+const buttonIds=['#first','#second','#third'];
 
-const buttons=document.querySelectorAll('button');
-let list=['rock','paper','scissors'];
-// let buttonResults=[firstButton,secondButton,thirdButton];
+ let buttonResults=[rock,paper,scissors];
+for(let i=0;i < 3;i++){
+    const buttons=document.querySelector(buttonIds[i]);
+    const playerChoice=list[i];
+    const buttonDiv=buttonResults[i];
+    buttons.addEventListener('click',function(){
+        buttonDiv.textContent=playRound(playerChoice);
+    })
+}
 
-const btn0=document.querySelector("#first");
-btn0.addEventListener('click',function(){
-    rock.textContent=(playRound('rock'));
-})
-const btn1=document.querySelector("#second");
-btn1.addEventListener('click',function(){
-    paper.textContent=(playRound('paper'));
-})
-const btn2=document.querySelector("#third");
-btn2.addEventListener('click',function(){
-    scissors.textContent=(playRound('scissors'));
-})
-
-  div.append(rock,paper,scissors);
+ div.append(rock,paper,scissors);
 
